bd69a59c0af3d0d8a5aa52696d642203cbf6f102
Cameron <cst1493@gmail.com>
2020-09-06 18:41:00 -0500

    New navigation.

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,5 +1,26 @@
 // @ts-check
-import * as React from 'react';
-import { Text, View, Alert } from 'react-native';
+//import * as React from 'react';
+//import { Text, View, Alert } from 'react-native';
 //import getEnvVars from './xxxxx';
 //import { Card, Button } from 'react-native-elements';
+
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+ 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+ 
+const FavoritesScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Favorites Screen</Text>
+    </View>
+  );
+};
+ 
+export default FavoritesScreen;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,33 @@
 // @ts-check
-import * as React from 'react';
-import { Text, View, Alert, ScrollView } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+ 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+ 
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Protected Home Screen</Text>
+    </View>
+  );
+};
+ 
+export default HomeScreen;
+
+//import * as React from 'react';
+//import { Text, View, Alert, ScrollView } from 'react-native';
 //import getEnvVars from './xxxxx';
 //import { Card, Button } from 'react-native-elements';
 
-export default function HomeScreen({navigation}) {
+/*export default function HomeScreen({navigation}) {
     return (
-      <View /*style={styles.container}*/>
+      <View>
         <ScrollView>
           <View>
               
@@ -18,4 +39,4 @@ export default function HomeScreen({navigation}) {
 
   HomeScreen.navigationOptions = {
     header: null,
-  };
+  };*/
