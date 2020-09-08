a746d86c337ae1cc0813b7cca495f8561e9d9857
Kurt <kurt.kiley@selu.edu>
2020-09-07 20:31:25 -0500

    Update RandomizerScreen.js
    Added a simple roulette wheel to the randomizer screen.

--- screens/RandomizerScreen.js
+++ screens/RandomizerScreen.js
@@ -1,6 +1,7 @@
 // @ts-check
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
+import { Wheel } from 'react-custom-roulette';
  
 const styles = StyleSheet.create({
   container: {
@@ -9,14 +10,31 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const data = [
+    { option: 'Pizza', style: { backgroundColor: 'green', textColor: 'black' } },
+    { option: 'Hamburger', style: { backgroundColor: 'white', textColor: 'black' } },
+    { option: 'Salad', style: {backgroundColor: 'blue', textColor: 'black'} },
+    { option: 'Chicken', style: { backgroundColor: 'orange', textColor: 'black' } },
+    { option: 'Spaghetti' },
+    { option: 'Spin Again' },
+]
  
 const RandomizerScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
+      <Wheel
+        mustStartSpinning={true}
+        prizeNumber={Math.floor(Math.random() * (data.length - 1))}
+        data={data}
+        backgroundColors={['#3e3e3e', '#df3428']}
+        textColors={['#ffffff']}
+      />
       <Text>Spin-Wheel/Randomizer Screen</Text>
+      
       <Button
-      title="Back to Favorites"
-      onPress={() => navigation.navigate('Favorites')}
+        title="Back to Favorites"
+        onPress={() => navigation.navigate('Favorites')}
       />
     </View>
   );
