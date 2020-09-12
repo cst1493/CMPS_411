b6bd7464a873623ad8ddbf76294e1c0b57bed93e
Cameron <cst1493@gmail.com>
2020-09-12 00:34:01 -0500

    Trying something. If starting, pull prev version
    npm i react-native-wheel-of-fortune --save
    npm install react-native-svg
    npm i d3-shape

--- screens/RandomizerScreen.js
+++ screens/RandomizerScreen.js
@@ -1,7 +1,39 @@
 // @ts-check
 import React from 'react';
+//import * as React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
- 
+import { Value } from 'react-native-reanimated';
+import WheelOfFortune from 'react-native-wheel-of-fortune';
+
+
+var value;
+
+const RandomizerScreen = ({ navigation }) => {
+  return (
+    DumbWheel()
+  );
+};
+
+function DumbWheel(){
+  var rewards = [1,2,3,4,5,6,7,8,9,10]
+  return(
+  <View style={styles.container}>
+      <WheelOfFortune
+          onRef={ref => (WheelOfFortune.child = ref)} 
+          rewards={ rewards }
+          knobSize={20}
+          borderWidth={3}
+          borderColor={"#FFF"}
+          winner={3}
+          innerRadius={50}
+          backgroundColor={"#c0392b"}
+          getWinner={(value, index) => WheelOfFortune.setState({ winnerValue: value, winnerIndex: index })}
+      />
+      <Button title="Press me" onPress={ () => { WheelOfFortune.child._onPress() } } />
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -9,17 +41,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
- 
-const RandomizerScreen = ({ navigation }) => {
-  return (
-    <View style={styles.container}>
-      <Text>Spin-Wheel/Randomizer Screen</Text>
-      <Button
-      title="Back to Favorites"
-      onPress={() => navigation.navigate('Favorites')}
-      />
-    </View>
-  );
-};
- 
+
 export default RandomizerScreen;
