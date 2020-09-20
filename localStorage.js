7c6d88b85eefc7f821fcfa20a1b81cc1d23d2b6b
Cameron <cst1493@gmail.com>
2020-09-20 18:19:32 -0500

    localStorage V2

--- localStorage.js
+++ localStorage.js
@@ -4,50 +4,43 @@ import { Text, Button } from 'react-native';
 import { AsyncStorage } from 'react-native';
 
 class localStorage extends Component{
-    state = {
-        testKey: '',
-        //foodTitles: '',
-
-    }
-
-    constructor(props) {
-        super(props);
-        this._retrieveData();
-    }
     
-    //static setItem(key: string, value: string, [callback]: ?(error: ?Error) => void)
-    _storeData = async () => {
-        try {
-            this.setState({ testKey: 'testStoredValue' })
-            await AsyncStorage.setItem('testKey', 'testStoredValue')
-        } catch (error) {
-          console.log("catch localStorage.async _storeData");
-        }
-    };
-    
-    _retrieveData = async () => {
-        try {
-          const value = await AsyncStorage.getItem('testKey');
-
-          if (value !== null) {
-            //We have data
-            console.log(value);
-            this.setState({ testKey: value })
-          }
-        } catch (error) {
-            console.log("catch localStorage._retrieveData");
-        }
-    };
-
-
-    //await AsyncStorage.removeItem('removedKey');
-    //await AsyncStorage.clear() //wipes all local data.
-
-    //export default _retrieveData; //import in HomeScreen.js
-
-
 }
 
+const testingFavorites = ['Chicken', 'Fish', 'Burgers', 'Pizza', 'Salad', 'Shrimp', 'China Buffet',
+    'Popeyes', 'BK', 'Canes', 'Subway', 'Fried Rice', 'Tacos', 'Pancakes', 'Eggs'];
+
+const favoritesKey = 'favorites';
+var favoritesArray = ['temp', 'temp'];
 
 
+async function storeData() {
+    favoritesArray = testingFavorites; //TODO TEMP
+    try {
+        AsyncStorage.setItem(favoritesKey, JSON.stringify(favoritesArray));
+        console.log('stored: ' + favoritesArray);
+    } catch (error) {
+      console.log('catch localStorage.async storeData()');
+    }
+}
+
+async function retrieveData() {
+    try {
+      const favsList = await AsyncStorage.getItem(favoritesKey);
+
+      if (favsList !== null) { //if found any data
+        console.log(favsList);
+        return AsyncStorage.getItem(favsList)
+        .then(req => JSON.parse(req))
+        .then(json => console.log(json))
+        .catch(error => console.log('error!')); //stringArr[] = retrieveData(); || stringArr.retrieveData(); ???
+      }
+    } catch (error) {
+        console.log("catch localStorage.retrieveData()");
+    }
+    return 
+}
+export default {
+    favoritesArray, localStorage,
+}
 
