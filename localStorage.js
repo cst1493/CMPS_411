bfe060f132f626d67b6e25e29ba10da6ab975444
Cameron <cst1493@gmail.com>
2020-09-21 10:58:53 -0500

    minor changes and new color theme in Consts.js
    Any changes to homescreen or Consts.js can be discarded in the case of a merge conflict.

--- localStorage.js
+++ localStorage.js
@@ -2,23 +2,26 @@
 import React, { Component } from 'react';
 import { Text, Button } from 'react-native';
 import { AsyncStorage } from 'react-native';
+import Consts from './Consts';
 
 class localStorage extends Component{
-    
+    //favoritesArray['', ''];
+    favoritesArry = retrieveData()
 }
 
 const testingFavorites = ['Chicken', 'Fish', 'Burgers', 'Pizza', 'Salad', 'Shrimp', 'China Buffet',
     'Popeyes', 'BK', 'Canes', 'Subway', 'Fried Rice', 'Tacos', 'Pancakes', 'Eggs'];
 
 const favoritesKey = 'favorites';
-var favoritesArray = ['temp', 'temp'];
-
+//var favoritesArray = ['temp', 'temp'];
+var favoritesArray;
 
 async function storeData() {
     favoritesArray = testingFavorites; //TODO TEMP
     try {
         AsyncStorage.setItem(favoritesKey, JSON.stringify(favoritesArray));
         console.log('stored: ' + favoritesArray);
+        return
     } catch (error) {
       console.log('catch localStorage.async storeData()');
     }
@@ -35,6 +38,7 @@ async function retrieveData() {
         .then(json => console.log(json))
         .catch(error => console.log('error!')); //stringArr[] = retrieveData(); || stringArr.retrieveData(); ???
       }
+      else console.log('favoritesKey is empty.')
     } catch (error) {
         console.log("catch localStorage.retrieveData()");
     }
