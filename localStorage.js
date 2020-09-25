73d5999a10bac0803d2f0a4341a94bedc8ee449b
estewart413 <dane.stewart@selu.edu>
2020-09-25 15:20:03 -0500

    Created Method to move favorite to bottom of list
    -installed array-move dependency
    -created MoveToBotOfList method in localStorage.js

---
+++ localStorage.js
@@ -0,0 +1,82 @@
+/// @ts-check
+import React, { Component } from 'react';
+import { Text, Button } from 'react-native';
+import { AsyncStorage } from 'react-native';
+import Consts from './Consts';
+import arrayMove from 'array-move';
+
+class localStorage extends Component{
+    //favoritesArray['', ''];
+    favoritesArry = retrieveData()
+}
+
+const testingFavorites = ['Chicken', 'Fish', 'Burgers', 'Pizza', 'Salad', 'Shrimp', 'China Buffet',
+    'Popeyes', 'BK', 'Canes', 'Subway', 'Fried Rice', 'Tacos', 'Pancakes', 'Eggs'];
+
+const favoritesKey = 'favorites';
+//var favoritesArray = ['temp', 'temp'];
+var favoritesArray;
+
+async function storeData() {
+    favoritesArray = testingFavorites; //TODO TEMP
+    try {
+        AsyncStorage.setItem(favoritesKey, JSON.stringify(favoritesArray));
+        console.log('stored: ' + favoritesArray);
+        return
+    } catch (error) {
+      console.log('catch localStorage.async storeData()');
+    }
+}
+
+async function retrieveData() {
+    try {
+      const favsList = await AsyncStorage.getItem(favoritesKey);
+
+      if (favsList !== null) { //if found any data
+        console.log(favsList);
+        return AsyncStorage.getItem(favsList)
+        .then(req => JSON.parse(req))
+        .then(json => console.log(json))
+        .catch(error => console.log('error!')); //stringArr[] = retrieveData(); || stringArr.retrieveData(); ???
+      }
+      else console.log('favoritesKey is empty.')
+    } catch (error) {
+        console.log("catch localStorage.retrieveData()");
+    }
+    return 
+}
+
+async function MoveToBotOfList(favorites, latestMeal) {
+    try {
+        //Get Last Index of Array
+        const len = favorites.length-1;
+
+        //Find index of latestMeal
+        var latestIndex;
+        for(var i = 0; i<favorites.length;i++){
+            if (favorites[i]==latestMeal) {
+                latestIndex = i;
+            } 
+        }
+        //check to make sure latestIndex isn't null
+        if (latestIndex == null) {
+            throw 'Index not found!';
+        }
+        //move the index to the last index
+        const newFavArray = arrayMove(favorites,latestIndex,len);
+        
+        //Store the new array in local storage
+        AsyncStorage.setItem(favoritesKey, JSON.stringify(newFavArray));
+        console.log('stored: ' + newFavArray);
+        return;
+
+
+    } catch(error) {
+        console.log("MoveToBotOfList" + error);
+    }
+}
+
+export default {
+    favoritesArray, localStorage,
+}
+
